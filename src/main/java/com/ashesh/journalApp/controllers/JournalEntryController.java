fc63a2ecb67b93e8ad70e48daf32117609d8aee8
ashesh-mandal <asheshmandal73@gmail.com>
2025-03-03 23:50:33 +0530

    updated

--- src/main/java/com/ashesh/journalApp/controllers/JournalEntryController.java
+++ src/main/java/com/ashesh/journalApp/controllers/JournalEntryController.java
@@ -19,52 +19,79 @@ public class JournalEntryController {
 	private JournalEntryService journalEntryService;
 
 	@PostMapping
-	public ResponseEntity<ResponseData> saveJournalEntry(@RequestParam String username, @RequestBody JournalEntry newJournalEntry) {
+	public ResponseEntity<ResponseData> saveJournalEntry(
+			@RequestParam String username,
+			@RequestBody JournalEntry newJournalEntry) {
 		try {
 			JournalEntry journalEntry = journalEntryService.saveJournalEntry(username, newJournalEntry);
-			return new ResponseEntity<>(new ResponseData("Journal entry added!", journalEntry), HttpStatus.CREATED);
-		}catch (Exception e){
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					new ResponseData("Journal entry added!", journalEntry),
+					HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping
-	public ResponseEntity<ResponseData> getAllJournalEntriesOfUser(@RequestParam String username){
+	public ResponseEntity<ResponseData> getAllJournalEntriesOfUser(@RequestParam String username) {
 		try {
 			List<JournalEntry> journalEntries = journalEntryService.getAllJournalEntries(username);
-			return new ResponseEntity<>(new ResponseData("Journal entries fetched!", journalEntries), HttpStatus.OK);
+			return new ResponseEntity<>(
+					new ResponseData("Journal entries fetched!", journalEntries),
+					HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<ResponseData> getJournalEntry(@PathVariable ObjectId id){
+	public ResponseEntity<ResponseData> getJournalEntry(@PathVariable ObjectId id) {
 		try {
 			JournalEntry journalEntry = journalEntryService.getJournalEntryById(id);
-			return new ResponseEntity<>(new ResponseData("Journal entry fetched!", journalEntry), HttpStatus.OK);
-		}catch (Exception e){
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					new ResponseData("Journal entry fetched!", journalEntry),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<ResponseData> updateJournalEntry(@PathVariable ObjectId id, @RequestParam String username, @RequestBody JournalEntry newJournalEntry){
+	public ResponseEntity<ResponseData> updateJournalEntry(
+			@PathVariable ObjectId id,
+			@RequestParam String username,
+			@RequestBody JournalEntry newJournalEntry) {
 		try {
 			JournalEntry journalEntry = journalEntryService.updateJournalEntry(id, newJournalEntry, username);
-			return new ResponseEntity<>(new ResponseData("Journal entry updated!", journalEntry), HttpStatus.OK);
-		}catch (Exception e){
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					new ResponseData("Journal entry updated!", journalEntry),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> deleteJournalEntry(@PathVariable ObjectId id, @RequestParam String username){
+	public ResponseEntity<?> deleteJournalEntry(
+			@PathVariable ObjectId id,
+			@RequestParam String username) {
 		try {
 			journalEntryService.deleteJournalEntry(id, username);
-			return new ResponseEntity<>(new ResponseData("Journal entry deleted!", null), HttpStatus.OK);
-		}catch (Exception e){
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					new ResponseData("Journal entry deleted!", null),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
--- src/main/java/com/ashesh/journalApp/controllers/UserController.java
+++ src/main/java/com/ashesh/journalApp/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.ashesh.journalApp.controllers;
 import com.ashesh.journalApp.entities.User;
 import com.ashesh.journalApp.services.UserService;
 import com.ashesh.journalApp.utils.ResponseData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
 	@Autowired
@@ -21,38 +23,57 @@ public class UserController {
 	public ResponseEntity<ResponseData> addUser(@RequestBody User user) {
 		try {
 			User newUser = userService.saveUser(user);
-			return new ResponseEntity<>(new ResponseData("User added!", newUser), HttpStatus.CREATED);
+			return new ResponseEntity<>(
+					new ResponseData("User added!", newUser),
+					HttpStatus.CREATED);
 		} catch (Exception e) {
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			log.error("Error while adding user: {}", e.getMessage());
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<ResponseData> login(@RequestBody User user){
+	public ResponseEntity<ResponseData> login(@RequestBody User user) {
 		try {
-			return new ResponseEntity<>(new ResponseData("User logged in!", userService.login(user)), HttpStatus.OK);
+			return new ResponseEntity<>(
+					new ResponseData("User logged in!", userService.login(user)),
+					HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping("/{username}")
-	public ResponseEntity<ResponseData> getUser(@PathVariable String username){
+	public ResponseEntity<ResponseData> getUser(@PathVariable String username) {
 		try {
 			User user = userService.getUserByUsername(username);
-			return new ResponseEntity<>(new ResponseData("User fetched!", user), HttpStatus.OK);
-		}catch (Exception e){
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					new ResponseData("User fetched!", user),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@PutMapping("/{username}")
-	public ResponseEntity<ResponseData> updateJournalEntry(@PathVariable String username,  @RequestBody User newUser){
+	public ResponseEntity<ResponseData> updateJournalEntry(
+			@PathVariable String username,
+			@RequestBody User newUser) {
 		try {
 			userService.updateUser(username, newUser);
-			return new ResponseEntity<>(new ResponseData("User updated!", newUser), HttpStatus.OK);
-		}catch (Exception e){
-			return new ResponseEntity<>(new ResponseData(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					new ResponseData("User updated!", newUser),
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(
+					new ResponseData(e.getMessage(), null),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
